f3d00c062e2e94142fdc9bc42aef47dd72d4195e
Yusuke Yamamoto <yusuke@mac.com>
2015-05-30 14:00:29 +0900

    だいたい完成

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yusuke
+ * Date: 5/30/15
+ * Time: 13:57
+ */
+
+$person = new Person("hoge", 18);
+
+echo $person->getName();
+if ($person->getName() != null) {
+
+}
+
+if ($person->isYoung()) {
+
+}
