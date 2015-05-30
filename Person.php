22e2e5d308ebfb36e49748243cb0cecec3c1376e
Yusuke Yamamoto <yusuke@mac.com>
2015-05-30 15:42:42 +0900

    書いたよ！

---
+++ Person.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: yusuke
+ * Date: 5/30/15
+ * Time: 15:39
+ */
+class Person
+{
+    protected $name;
+    protected $age;
+
+    /**
+     * Person constructor.
+     * @param $name
+     * @param $age
+     */
+    public function __construct($name, $age)
+    {
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    public function isYoung()
+    {
+        return $this->getAge() < 20;
+    }
+
+
+}
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yusuke
+ * Date: 5/30/15
+ * Time: 15:40
+ */
+
+$person = new Person("yusuke", 18);
+
+if ($person->isYoung()) {
+
+}
+
+if ($person->getName() != null) {
+
+}
