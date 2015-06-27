5d15d51db9d85dd75f445cefa35db96b3de8be3a
Yusuke Yamamoto <yusuke@mac.com>
2015-06-27 15:45:03 +0900

    asd;lfjasd;lasdjfawoifjweaopi

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 <?php
+?>
+
+<style type="text/css">
+    .hello {
+        font-size: 100pt;
+        color: #1efffc;
+    }
+</style>
+<div class="hello">hello world</div>
+
+<script src="js.js"></script>
+<script>
+    document.write(kakeru(100, 2));
+</script>
