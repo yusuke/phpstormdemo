bc369ba94dd9f5ae80656dc8458597fc51982393
Yusuke Yamamoto <yusuke@mac.com>
2015-06-27 15:40:34 +0900

    asd;lfjasd;lasdjfawoifjweaopi

---
+++ Person.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: yusuke
+ * Date: 6/27/15
+ * Time: 15:33
+ */
+interface Person
+{
+    function getName();
+
+    function getAge();
+
+    function isYoung();
+}
